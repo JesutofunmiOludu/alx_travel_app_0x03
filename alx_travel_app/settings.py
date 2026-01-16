3f42c677c959601dd07e5a5e3f6aaac146be6f32
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2026-01-17 00:22:53 +0100

    Update

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 """
 Django settings for alx_travel_app project.
@@ -111,10 +112,11 @@ WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 # Chapa Payment Gateway Configuration
